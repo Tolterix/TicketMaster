a9fa4e02a4360b741ae6c337d94a70429f0bbadd
jaredshue <74428951+jaredshue@users.noreply.github.com>
2020-12-17 15:52:21 -0500

    Update login.spec.js

--- frontend/cypress/integration/Login/login.spec.js
+++ frontend/cypress/integration/Login/login.spec.js
@@ -8,24 +8,29 @@ describe('Login page', () => {
     });
 
     describe('#login-form', () => {
+        var loginForm;
+        beforeEach(() => {
+            loginForm = cy.get('form#login-form');
+        });
+
         it('should contain a label for email', () => {
-            const loginForm = cy.get('form#login-form');
             loginForm.get('label[for="email"]').should('contain', 'Email:');
         });
 
         it('should contain a text input for email', () => {
-            const loginForm = cy.get('form#login-form');
             loginForm.get('input#email[name="email"]').should('exist');
         });
 
         it('should contain a label for password', () => {
-            const loginForm = cy.get('form#login-form');
             loginForm.get('label[for="password"]').should('contain', 'Password:');
         });
 
         it('should contain a password input for password', () => {
-            const loginForm = cy.get('form#login-form');
             loginForm.get('input#password[name="password"]').should('exist');
+        });
+
+        it('should contain a button to login', () => {
+            loginForm.get('button#login').should('exist');
         });
     });
 });
