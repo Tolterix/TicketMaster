5a1636df4e16e33ad7d6a8a82b4d3f2c66fa0890
jaredshue <74428951+jaredshue@users.noreply.github.com>
2020-12-17 16:35:00 -0500

    Update login.spec.js

--- frontend/cypress/integration/Login/login.spec.js
+++ frontend/cypress/integration/Login/login.spec.js
@@ -8,29 +8,24 @@ describe('Login page', () => {
     });
 
     describe('#login-form', () => {
-        var loginForm;
-        beforeEach(() => {
-            loginForm = cy.get('form#login-form');
-        });
-
         it('should contain a label for email', () => {
-            loginForm.get('label[for="email"]').should('contain', 'Email:');
+            cy.get('#login-form > label[for="email"]').should('contain', 'Email:');
         });
 
         it('should contain a text input for email', () => {
-            loginForm.get('input#email[name="email"]').should('exist');
+            cy.get('#login-form > input#email[name="email"]').should('exist');
         });
 
         it('should contain a label for password', () => {
-            loginForm.get('label[for="password"]').should('contain', 'Password:');
+            cy.get('#login-form > label[for="password"]').should('contain', 'Password:');
         });
 
         it('should contain a password input for password', () => {
-            loginForm.get('input#password[name="password"]').should('exist');
+            cy.get('#login-form > input#password[name="password"]').should('exist');
         });
 
         it('should contain a button to login', () => {
-            loginForm.get('button#login').should('exist');
+            cy.get('#login-form > input#login[type="submit"][value="Login"]').should('exist');
         });
     });
 });
