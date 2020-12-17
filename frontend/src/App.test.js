d63f8a30c6b8afdcb925644b7e012dc8f0fdacaf
jaredshue <74428951+jaredshue@users.noreply.github.com>
2020-12-17 16:35:12 -0500

    Update App.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,6 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { shallow } from 'enzyme';
+
 import App from './App';
 
-test('', () => {
-  // ...
+describe('<App />', () => {
+    it('', () => {
+        // ...
+    });
 });
