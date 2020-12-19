f28a1ab49253f3e153dd02eafc8583f97131a5c8
adder206 <40439204+adder206@users.noreply.github.com>
2020-12-19 10:27:16 -0800

    Added NavBar and NavBar CSS. Not wired to App.js

--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -1,5 +1,64 @@
-const NavigationBar = () => {
-    
-}
+import { Component } from 'react';
+import './NavigationBar.css';
+import AirForceLogo from '../AirForceLogo.png';
 
-export default NavigationBar;
+
+function NavigationBar() {
+    // const [sidebar, setSidebar] = useState(false)
+    // const showSidebar = () => setSidebar(!sidebar)
+    return (
+        <div className='navbar-grid'> 
+            <nav className='nav-menu'>
+                <ul className='nav-menu-items'>
+                    <div class="box-a">
+                        <li className='airforce-logo'> 
+                            <a href='/'>
+                                <img src = {AirForceLogo}
+                                alt='Air Force Logo'/>;
+                            </a>
+                        </li>
+                    </div>
+                    <div class="box-b">
+                        <li className="nav-text"> 
+                            <a href="/tickets">
+                                View Tickets
+                            </a>
+                        </li>
+                     </div>
+                    <div class="box-c">
+                        <li className="nav-text">
+                            <a href="/tickets/submit">
+                                Submit Ticket
+                            </a>
+                        </li>
+                    </div>
+                    <div class="box-d">
+                        <li className="nav-text">
+                            <a href="/profile">
+                                Profile
+                            </a>
+                        </li>
+                    </div>
+                    <div class="box-e">
+                        <li className="nav-text">
+                            <a href="/workcenter">
+                                Work Center
+                            </a>
+                        </li>
+                    </div>
+                    <div class="box-f">
+                    <li className="nav-text">
+                        <a href="/logout">
+                            Log Out
+                        </a>
+                    </li>
+                    </div>
+                </ul>
+            </nav>
+      </div>
+    );
+
+
+
+}
+export default NavigationBar; 
