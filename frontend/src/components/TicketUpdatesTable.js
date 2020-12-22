b34e87886fc6ce3860f845ea1cac92bba67e295e
jaredshue <>
2020-12-22 15:58:08 -0500

    Update TicketUpdatesTable.js

--- frontend/src/components/TicketUpdatesTable.js
+++ frontend/src/components/TicketUpdatesTable.js
@@ -4,7 +4,12 @@ import { StateContext } from '../State';
 const TicketUpdatesTable = () => {
     const context = React.useContext(StateContext);
 
-    var ticketUpdates;
+    var ticketUpdates = [
+        { description: "Contacted customer", updatedBy: "Jared Shue", updatedAt: "Today" },
+        { description: "Contacted customer", updatedBy: "Jared Shue", updatedAt: "Today" },
+        { description: "Contacted customer", updatedBy: "Jared Shue", updatedAt: "Today" },
+        { description: "Contacted customer", updatedBy: "Jared Shue", updatedAt: "Today" },
+    ];
     React.useEffect(async () => {
         var response = await fetch(
             `http://localhost:8080/tickets/${context.tickets.selected}`
@@ -14,27 +19,32 @@ const TicketUpdatesTable = () => {
     });
 
     return (
-        <table>
-            <h1>Updates</h1>
-            <tr>
-                <th>Description</th>
-                <th>Updated By</th>
-                <th>Updated At</th>
-            </tr>
-            {
-                !ticketUpdates
-                ? null
-                : ticketUpdates.map((update) => {
-                    return (
-                        <tr>
-                            <td>{ update.description }</td>
-                            <td>{ update.updatedBy }</td>
-                            <td>{ update.updatedAt }</td>
-                        </tr>
-                    );
-                })
-            }
-        </table>
+        <div className="table-responsive">
+            <table className="table table-bordered" width="100%" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th>Description</th>
+                        <th>Updated By</th>
+                        <th>Updated At</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        !ticketUpdates
+                        ? null
+                        : ticketUpdates.map((update) => {
+                            return (
+                                <tr>
+                                    <td>{ update.description }</td>
+                                    <td>{ update.updatedBy }</td>
+                                    <td>{ update.updatedAt }</td>
+                                </tr>
+                            );
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
     );
 }
 
