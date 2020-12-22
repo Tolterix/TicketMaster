9104d24ef8548c4743dac5d4992f269b03c02b42
jaredshue <>
2020-12-22 15:57:26 -0500

    Update navigation bar

--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -1,73 +1,89 @@
-import React from 'react';
-import { Redirect } from 'react-router-dom';
-import './NavigationBar.css';
-import AirForceLogo from './AirForceLogo.png';
-import { StateContext } from '../State';
-
+import React from "react";
+import { Redirect } from "react-router-dom";
+import { StateContext } from "../State";
 
 function NavigationBar() {
     const context = React.useContext(StateContext);
 
     const handleClick = (event) => {
-        event.preventDefault()
+        event.preventDefault();
 
-        context.setState({
-            navbarSwitch: 1,
-            navbarRedirect: ('/' + event.target.name)
-        })
+        if (!window.location.toString().endsWith(event.target.name)
+            && context.navbarSwitch === 0) {
+                context.setState({
+                    navbarSwitch: 1,
+                    navbarRedirect: `/${event.currentTarget.name}`
+                });
+        }
     }
 
-    if (window.location.toString().includes(context.navbarRedirect) && context.navbarSwitch == 1) {
+    if (window.location.toString().endsWith(context.navbarRedirect)
+        && context.navbarSwitch === 1) {
         context.setState({
             navbarSwitch: 0,
-            navbarRedirect: '/'
-        })
+            navbarRedirect: "/"
+        });
     }
 
     return (
-        <div className='navbar-grid'>
-            <nav className='nav-menu'>
-                <ul className='nav-menu-items'>
-                    {
-                        context.navbarSwitch == 0 ? <div></div>
-                        : <Redirect to={context.navbarRedirect}> Logout </Redirect>
-                    }
-                    <li className='airforce-logo'>
-                        <button className='navbarButton' onClick={handleClick} name=''>
-                            <img src = {AirForceLogo} alt='Air Force Logo'/>
-                        </button>
-                    </li>
-                    <li className="nav-text">
-                        <button className='navbarButton' onClick={handleClick} name='tickets'>
-                            View Tickets
-                        </button>
-                    </li>
-                    <li className="nav-text">
-                        <button className='navbarButton' onClick={handleClick} name='tickets/submit'>
-                            Submit Ticket
-                        </button>
-                    </li>
-                    <li className="nav-text">
-                        <button className='navbarButton' onClick={handleClick} name='profile'>
-                            Profile
-                        </button>
-                    </li>
-                    <li className="nav-text">
-                        <button className='navbarButton' onClick={handleClick} name='workcenter'>
-                            Work Center
-                        </button>
-                    </li>
-                    <li className="nav-text">
-                        <button className='navbarButton' onClick={handleClick} name='logout'>
-                            Log Out
-                        </button>
-                    </li>
-                </ul>
-            </nav>
-      </div>
+        <>
+            {
+                context.navbarSwitch === 0
+                ? null
+                : <Redirect to={ context.navbarRedirect } />
+            }
+            <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion">
+                <a className="sidebar-brand d-flex align-items-center justify-content-center">
+                    <div className="sidebar-brand-text mx-3">
+                        TicketMaster
+                    </div>
+                </a>
+
+                <hr className="sidebar-divider" />
+
+                <div className="sidebar-heading">
+                    Tickets
+                </div>
+
+                <li className="nav-item active">
+                    <a className="nav-link" name="tickets" onClick={ handleClick }>
+                        <span>View Tickets</span>
+                    </a>
+                </li>
+
+                <li className="nav-item">
+                    <a className="nav-link" name="tickets/submit" onClick={ handleClick }>
+                        <span>Submit a Ticket</span>
+                    </a>
+                </li>
+
+                <hr className="sidebar-divider" />
+
+                <div className="sidebar-heading">
+                    Profile
+                </div>
+
+                <li className="nav-item">
+                    <a className="nav-link" name="profile" onClick={ handleClick }>
+                        <span>My Profile</span>
+                    </a>
+                </li>
+
+                <li className="nav-item">
+                    <a className="nav-link" name="workcenter" onClick={ handleClick }>
+                        <span>My Workcenter</span>
+                    </a>
+                </li>
+
+                <hr className="sidebar-divider" />
+
+                <li className="nav-item">
+                    <a className="nav-link" name="logout" onClick={ handleClick }>
+                        <span>Logout</span>
+                    </a>
+                </li>
+            </ul>
+        </>
     );
-
-
-
 }
 export default NavigationBar;
